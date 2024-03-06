052c88400ab93aa7ee2cfee0874cb7789a5fb10a
Can Kartal <can.krtl05@gmail.com>
2024-03-07 01:59:40 +0300

    Değişiklikler yapıldı: Yeni dosyalar eklendi

---
+++ degiskenler.dart
@@ -0,0 +1,10 @@
+void main(){
+  String mesaj = "Merhaba Can";
+  int dogumYili = 2000;
+  double oran = 1.15;
+  bool loginMi = false;
+
+  print(mesaj);
+  print("Doğum yılı: " + dogumYili.toString());
+  print("Oran: " + oran.toString());
+}
